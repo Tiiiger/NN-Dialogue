4eb5e952ced6bcec9383c7ae77d223ec42540dce
Tiiiger <zty27x@gmail.com>
2018-03-10 17:18:51 -0500

    "skethcing the seq2seq model."

--- seq2seq/seq.py
+++ seq2seq/seq.py
@@ -3,3 +3,43 @@ import torch.nn as nn
 from torch.autograd import Variable
 from torch import optim
 import torch.nn.functional as F
+from data import parse
+from data import Loader
+
+params = parse()
+train_loader = Loader(params)
+test_loader = Loader(params, "../Data/t_given_s_test.txt")
+
+def masked_cross_entropy_loss():
+    raise NotImplemented
+
+class Encoder(nn.Module):
+
+    def __init__(self, params):
+        super(Encoder, self).__init__()
+        raise NotImplemented
+
+    def forward(self):
+        raise NotImplemented
+
+    def save_model(self, path):
+        raise NotImplemented
+
+class Decoder(nn.module):
+
+    def __init__(self, params):
+        super(Decoder, self).__init__()
+        raise NotImplemented
+
+    def forward(self):
+        raise NotImplemented
+
+def train():
+    raise NotImplemented
+
+def test():
+    raise NotImplemented
+
+def evaluate():
+    raise NotImplemented
+
