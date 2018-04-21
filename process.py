ea36dc101f9b887faa6289499665eb6a347561f6
Tiiiger <zty27x@gmail.com>
2018-04-21 18:25:33 +0000

    too show. just use grep.

--- process.py
+++
@@ -1,9 +0,0 @@
-processed = []
-with open("OpenSubData/t_given_s_dialogue_length2_6.txt", "w") as f:
-    for line in f.readlines():
-        _, t = line.split('l')
-        t = t.split()
-        if len(t) < 20:
-            processed.append(line)
-with open("processed/t_given_s_dialogue_length2_6_19.txt", "w") as to:
-    to.writelines(processed)
